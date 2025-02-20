79ca4d4e36284110506e98a3091f2e136651ba71
Valery1959 <valery.olshansky1959@gmail.com>
2025-02-20 11:06:39 +0300

    Add root size option, add extra partition name option, add boot flag option

--- install_iso.sh
+++ install_iso.sh
@@ -46,13 +46,30 @@ user=$3; [ -z $user ] && { usage; exit -1; }
 pass=$4
 
 # set up size of root partition
-typeset -i rsize=$5; [ $rsize -ne 0 ] && rp=yes; [ $rp ] && rs="+${rsize}G" || rs="-${rsize}"
+typeset -i rsize=$5; [ $rsize -ne 0 ] && rs="+${rsize}G" || rs="-${rsize}"
+
+# set up optional extra partition with name passed
+extra=$6
+
+# set up boolean flag to create optional boot partion
+[ ! -z $7 ] && create_boot=1
+
+# partitions, n is incremental count started form 1
+# /dev/sdXn - mandatory - EFI, 550M default size
+# /dev/sdXn - optional  - boot, 1G  default size
+# /dev/sdXn - mandatory - root, size is optional, size is til the end of disk by defalt
+# /dev/sdXn - optional  - <name>,  mounted to /, size is til the end of disk
 
 echo "$disks" | grep -E "^${disk}$" > /dev/null 2>&1
 [ $? -ne 0 ] && { echo "Disk $disk is not found"; exit -1; }
 
+# Check if disk is empty
 lsblk -n -l -o type,name | grep -E "^part\s+$disk" > /dev/null 2>&1
-#[ $? -eq 0 ] && { echo "Disk $disk has partitions, remove them manually before run"; exit -1; }
+if [ $? -eq 0 ] ; then
+  lsblk /dev/$disk -o "name,partlabel,label,size,fsused,UUID,model"
+  read -r -p "Disk $disk has partitions (see above), remove them? (y|n)" answer
+  [[ $answer == [yY] ]] || exit 1
+fi
 
 # get local timezone
 tz_ipapi="https://ipapi.co/timezone"
@@ -97,13 +114,23 @@ disk="/dev/$disk"
 par1=${disk}${p}1
 par2=${disk}${p}2
 par3=${disk}${p}3
-#par4=${disk}${p}4
+par4=${disk}${p}4
 
 echo "Disk $disk will be partitioned as follows"
-echo " EFI: $par1 : vfat"
-#echo "BOOT: $par2 : ext4"
-echo "ROOT: $par2 : ext4"
-[ $rp ] && echo "shared: $par3 : ext4"
+if [ $create_boot ] ; then
+  echo " EFI: $par1 : vfat"
+  echo "BOOT: $par2 : ext4"
+  echo "ROOT: $par3 : ext4"
+  expar=$par4
+  exnum=4
+else
+  echo " EFI: $par1 : vfat"
+  echo "ROOT: $par2 : ext4"
+  expar=$par3
+  exnum=3
+fi
+
+[ $extra ] && echo "$extra: $expar : ext4"
 
 # formatting disk, as follows:
 # 1. Zap (destroy) the GPT and MBR data structures
@@ -118,20 +145,44 @@ echo "Format $disk and mound partitions"
 run sgdisk -Z ${disk}
 run sgdisk -a 2048 -o ${disk} 
 run sgdisk -n 1::+550M -t 1:ef00 -c 1:EFI ${disk}
-#run sgdisk -n 2::+1G   -t 2:8300 -c 2:BOOT ${disk}
-run sgdisk -n 2::${rs} -t :8300 -c 2:ROOT ${disk}
-[ $rp ] && run sgdisk -n 3::-0 -t 3:8300 -c 3:shared ${disk}
+
+if [ $create_boot ] ; then
+  run sgdisk -n 2::+1G   -t 2:8300 -c 2:BOOT ${disk}
+  run sgdisk -n 3::${rs} -t 3:8300 -c 3:ROOT ${disk}
+else
+  run sgdisk -n 2::${rs} -t 2:8300 -c 2:ROOT ${disk}
+fi
+
+if [ $extra ] ; then
+  run sgdisk -n ${exnum}::-0 -t ${exnum}:8300 -c ${exnum}:${extra} ${disk}
+fi
+
 run partprobe ${disk} 
 
 run mkfs.fat -F32 -n EFI $par1
-#run mkfs.ext4 -L BOOT $par2
-run mkfs.ext4 -L ROOT $par2
-[ $rp ] && run mkfs.ext4 -L shared $par3
+if [ $create_boot ] ; then
+  run mkfs.ext4 -L BOOT $par2
+  run mkfs.ext4 -L ROOT $par3
+else
+  run mkfs.ext4 -L ROOT $par2
+fi
 
-run mount $par2 /mnt
-#run mount -m $par2 /mnt/boot
-run mount -m $par1 /mnt/boot/efi 
-[ $rp ] && run mount -m $par3 /mnt/shared
+if [ $extra ] ; then
+  run mkfs.ext4 -L $extra $expar
+fi
+
+if [ $create_boot ] ; then
+  run mount $par3 /mnt
+  run mount -m $par2 /mnt/boot
+  run mount -m $par1 /mnt/boot/efi
+else
+  run mount $par2 /mnt
+  run mount -m $par1 /mnt/boot/efi
+fi
+
+if [ $extra ] ; then
+  run mount -m $expar /mnt/$extra
+fi
 
 echo "Install essential packages (minimal)"
 run pacstrap -K /mnt base linux linux-firmware sudo
