3ccc5048a84aa38a0df0ffe3339ec8086ffe3ba7
Valery1959 <valery.olshansky1959@gmail.com>
2025-09-07 07:40:54 +0300

    Add btrfs

--- install_iso.sh
+++ install_iso.sh
@@ -51,24 +51,22 @@ typeset -i rsize=$5; [ $rsize -ne 0 ] && rs="+${rsize}G" || rs="-${rsize}"
 # set up optional extra partition with name passed, if size of root partition is passed
 extra=$6; [ $extra ] && [ $rsize -eq 0 ] && { echo "To create extra partition, you should pass size of root parttion"; exit 1; }
 
-# set up boolean flag to create optional boot partion
-[ ! -z $7 ] && create_boot=1
-
 # partitions, n is incremental count started form 1
 # /dev/sdXn - mandatory - EFI, 550M default size
-# /dev/sdXn - optional  - boot, 1G  default size
 # /dev/sdXn - mandatory - root, size is optional, size is til the end of disk by defalt
 # /dev/sdXn - optional  - <name>,  mounted to /, size is til the end of disk
 
 echo "$disks" | grep -E "^${disk}$" > /dev/null 2>&1
 [ $? -ne 0 ] && { echo "Disk $disk is not found"; exit -1; }
 
+dzap=1 # Zap (destroy) the GPT and MBR data structures on disk by default
+
 # Check if disk is empty
 lsblk -n -l -o type,name | grep -E "^part\s+$disk" > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
   lsblk /dev/$disk -o "name,partlabel,label,size,fsused,UUID,model"
   read -r -p "Disk $disk has partitions (see above), remove them? (y|n)" answer
-  [[ $answer == [yY] ]] || exit 1
+  [[ $answer == [yY] ]] || dzap= # keep disk partitions as is
 fi
 
 # get local timezone
@@ -114,74 +112,56 @@ disk="/dev/$disk"
 par1=${disk}${p}1
 par2=${disk}${p}2
 par3=${disk}${p}3
-par4=${disk}${p}4
 
 echo "Disk $disk will be partitioned as follows"
-if [ $create_boot ] ; then
-  echo " EFI: $par1 : vfat"
-  echo "BOOT: $par2 : ext4"
-  echo "ROOT: $par3 : ext4"
-  expar=$par4
-  exnum=4
-else
-  echo " EFI: $par1 : vfat"
-  echo "ROOT: $par2 : ext4"
-  expar=$par3
-  exnum=3
+echo " EFI: $par1 : vfat"
+echo "ROOT: $par2 : ext4"
+
+[ $extra ] && echo "$extra: $par3 : ext4"
+
+if [ $dzap ] ; then
+   # format disk as follows:
+   # 1. Zap (destroy) the GPT and MBR data structures
+   # 2. Create new gpt disk 2048 alignment
+   # 3. Create EFI partition
+   # 4. Create ROOT partition 
+   # 5. Inform the OS of partition table changes (partprobe)
+   # 6. Format EFI, and ROOT partitions
+   #    Notes: -c option is partlabel, -n and -L options are labels
+   echo "Format $disk and mound partitions"
+   run sgdisk -Z ${disk}
+   run sgdisk -a 2048 -o ${disk} 
+   run sgdisk -n 1::+550M -t 1:ef00 -c 1:EFI ${disk}
+   run sgdisk -n 2::${rs} -t 2:8300 -c 2:ROOT ${disk}
+
+   if [ $extra ] ; then
+     run sgdisk -n 3::-0 -t 3:8300 -c 3:${extra} ${disk}
+   fi
+
+   run partprobe ${disk} 
+
+   if [ $extra ] ; then
+      run mkfs.ext4 -L $extra $par3
+   fi
 fi
-
-[ $extra ] && echo "$extra: $expar : ext4"
-
-# formatting disk, as follows:
-# 1. Zap (destroy) the GPT and MBR data structures
-# 2. Create new gpt disk 2048 alignment
-# 3. Create EFI partition
-# 4. Create BOOT partition (kernels)
-# 5. Create ROOT partition 
-# 6. Inform the OS of partition table changes (partprobe)
-# 7. Format EFI, BOOT and ROT partitions
-#    Notes: -c option is partlabel, -n and -L options are labels
-echo "Format $disk and mound partitions"
-run sgdisk -Z ${disk}
-run sgdisk -a 2048 -o ${disk} 
-run sgdisk -n 1::+550M -t 1:ef00 -c 1:EFI ${disk}
-
-if [ $create_boot ] ; then
-  run sgdisk -n 2::+1G   -t 2:8300 -c 2:BOOT ${disk}
-  run sgdisk -n 3::${rs} -t 3:8300 -c 3:ROOT ${disk}
-else
-  run sgdisk -n 2::${rs} -t 2:8300 -c 2:ROOT ${disk}
-fi
-
-if [ $extra ] ; then
-  run sgdisk -n ${exnum}::-0 -t ${exnum}:8300 -c ${exnum}:${extra} ${disk}
-fi
-
-run partprobe ${disk} 
 
 run mkfs.fat -F32 -n EFI $par1
-if [ $create_boot ] ; then
-  run mkfs.ext4 -L BOOT $par2
-  run mkfs.ext4 -L ROOT $par3
-else
-  run mkfs.ext4 -L ROOT $par2
-fi
+#run mkfs.ext4 -L ROOT $par2
+run mkfs.btrfs $par2
+
+run mount $par2 /mnt
+run btrfs subvolume create /mnt/@
+run btrfs subvolume create /mnt/@home
+run umount /mnt
+
+run mount -o compress=zstd,subvol=@ $par2 /mnt
+#run mkdir -p /mnt/home
+run mount -m -o compress=zstd,subvol=@home $par2 /mnt/home
+
+run mount -m $par1 /mnt/boot/efi
 
 if [ $extra ] ; then
-  run mkfs.ext4 -L $extra $expar
-fi
-
-if [ $create_boot ] ; then
-  run mount $par3 /mnt
-  run mount -m $par2 /mnt/boot
-  run mount -m $par1 /mnt/boot/efi
-else
-  run mount $par2 /mnt
-  run mount -m $par1 /mnt/boot/efi
-fi
-
-if [ $extra ] ; then
-  run mount -m $expar /mnt/$extra
+  run mount -m $par3 /mnt/$extra
 fi
 
 echo "Init pacman keys and pupulate them from archlinux"
