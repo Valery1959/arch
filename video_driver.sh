dac74eb4239b6da88a3f13ae6b6993332cf33f44
Valery1959 <valery.olshansky1959@gmail.com>
2025-09-11 13:58:58 +0300

    fix

--- video_driver.sh
+++ video_driver.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-lspci -nnk | grep -E -i --color 'vga|3d|2d' -A3 | grep 'in use'
+lspci -k -d ::03xx
+
+echo "modeset:"
+sudo cat /sys/module/nvidia_drm/parameters/modeset
+echo "fbdev:"
+sudo cat /sys/module/nvidia_drm/parameters/fbdev
 
