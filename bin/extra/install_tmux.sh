3b3cf650df1eeb2a7a982dfeaaa45c6a16a2c01f
Valery1959 <valery.olshansky1959@gmail.com>
2025-09-17 10:00:29 +0300

    fix

---
+++ bin/extra/install_tmux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+script_dir=$(cd $(dirname $0); pwd -P)
+
+source $script_dir/../utils.sh
+
+[ ! -f $HOME/.zshrc ] && { echo "Install zsh first"; exit 1; }
+
+echo "Install tmux config"
+dir=$HOME/.config/tmux; [ -d $dir ] && run rm -rf $dir
+run mkdir -p $dir
+run cp $script_dir/config/tmux.conf $dir
+
+echo "Install tmux pluginmanager"
+tpm="$HOME/.tmux/plugins/tpm"; [ -d $tpm ] && run rm -rf $tpm
+run git clone https://github.com/tmux-plugins/tpm $tpm
+
+run rm -rf $tpm/.git
+run rm $tpm/.travis.yml
+run rm $tpm/.git*
+
+exit 0
+
